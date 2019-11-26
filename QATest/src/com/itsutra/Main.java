33579131ed474dc025fdd9f78c975ac3db365c03
SYUM\byasb <ajayabhatta40@gmail.com>
2019-11-26 11:20:59 -0600

    1stProject

---
+++ QATest/src/com/itsutra/Main.java
@@ -0,0 +1,12 @@
+package com.itsutra;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to my QA Environment");
+
+    }
+}
